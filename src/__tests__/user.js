7754b861e60202b7605b6e1bffd891ccc0d8bb71
Daniele <daniele.rb6@gmail.com>
2018-06-26 18:59:07 +0200

    test: Refactor broken test
    Refactored the test on the User component after the changes in code that broke it.

--- src/__tests__/user.js
+++ src/__tests__/user.js
@@ -12,11 +12,11 @@ jest.mock('../utils/utils');
  * Simulates an asynchronous behavior.
  * Adds the code that follows it to the end of the event loop queue.
  */
-function nextTick() {
+const flushPromises = () => {
   return new Promise(resolve => {
     setTimeout(resolve, 0);
   });
-}
+};
 
 // TODO
 //
@@ -30,11 +30,36 @@ function nextTick() {
 // });
 
 test('it returns the right username once the data has been fetched', async () => {
-  const wrapper = shallow(<User id={1} />);
+  const fakeUser = {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    address: {
+      city: 'Gwenborough'
+    },
+    phone: '1-770-736-8031 x56442',
+    company: {
+      name: 'Romaguera-Crona'
+    }
+  };
 
-  await nextTick();
+  const wrapper = shallow(<User user={fakeUser} />);
+
+  await flushPromises();
   wrapper.update();
 
-  const text = wrapper.find('p.id').text();
-  expect(text).toEqual('User 1');
+  console.log(
+    wrapper
+      .find('div')
+      .at(2)
+      .children()
+  );
+  // Here we are targeting the third div in the User component,
+  // that's the one that contains the 5 paragraphs.
+  // Remember that the result of 'find' is zero-based.
+  const thirdDiv = wrapper.find('div').at(2);
+  const paragraph = wrapper.find('p');
+  expect(thirdDiv.children()).toHaveLength(5);
+  expect(paragraph.first().text()).toEqual('Username: Bret');
+  expect(paragraph.at(3).text()).toEqual('City: Gwenborough');
 });
